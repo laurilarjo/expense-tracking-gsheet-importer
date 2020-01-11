b94293d3500c3fb3149bb15959b41c6666d6e5b0
Lauri Larjo <laurilarjo@gmail.com>
2020-01-11 21:58:20 +0100

    Almost ready.
    Also added launch.json for debugging.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,42 +1,38 @@
 import * as getRates from 'ecb-fx-rates';
 import moment = require('moment');
-//import  { ExchangeRate, Currencies, ExchangeResponse } from 'exchange-rates-as-promised';
+import  { ExchangeRate, Currencies, ExchangeResponse, HistoricalRates } from 'exchange-rates-as-promised';
 
 let currencyMap: Map<string, number> = new Map();
 
 async function convertSEKToEur(amount: number, date: string): Promise<number> {
-    
+
     const firstOfMonth: string = moment(date, 'D/M/YYYY').startOf('month').toString();
     const found = currencyMap.get(firstOfMonth);
-    let exchangeRate;
+    let SEK;
     if (!found) {
-        // TODO: This library uses old xlm2json, which has dependencies to deprecated packages. Should change.
         console.log(`Fetching SEK rate for date: ${firstOfMonth.toString()}`);
-        exchangeRate = await getRates({currency: 'SEK', date: firstOfMonth});
-        if (exchangeRate) {
-            currencyMap.set(firstOfMonth, exchangeRate);
+        //exchangeRate = await getRates({currency: 'SEK', date: firstOfMonth});
+        const exchangeRate = new ExchangeRate();
+        exchangeRate.setBaseCurrency(Currencies.EUR);
+        exchangeRate.setCurrencies([Currencies.SEK]);
+        exchangeRate.setDate(new Date(firstOfMonth));
+        const response = await exchangeRate.getRates();
+        SEK = response.rates.SEK;
+
+        if (SEK && typeof(SEK) == 'number') {
+            currencyMap.set(firstOfMonth, SEK);
         }
-        console.log(`Rate fetched for SEK: ${exchangeRate} for date: ${firstOfMonth.toString()}`);
+        else {
+            throw new Error('Could not find exchange rate for SEK for ' + firstOfMonth);
+        }
+        console.log(`Rate fetched for SEK: ${SEK} for date: ${response.date}. Wanted for date: ${firstOfMonth.toString()}`);
     } else {
-        exchangeRate = found;
+        SEK = found;
     }
-    return Math.round((amount / exchangeRate) * 100) / 100;
+    return Math.round((amount / SEK) * 100) / 100;
 }
 
-  /*
-    This exchangeRate library is not well done as NPM package, but is otherwise promising. 
-    I'm waiting for feedback on the issue: https://github.com/ToeFungi/exchange-rates-as-promised/issues/2
 
-    const exchangeRate = new ExchangeRate();
-    exchangeRate.setBaseCurrency(Currencies.EUR);
-    exchangeRate.setCurrencies([Currencies.SEK]);
-    exchangeRate.setDate(new Date());
-
-    exchangeRate.getRates().then((response: ExchangeResponse) => console.log({
-        base: response.base,
-        date: response.date,
-        rates: response.rates
-    }))
-    */
+    
 
 export { convertSEKToEur };
