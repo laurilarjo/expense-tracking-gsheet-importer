ec15bd9cd421c5b1989f6b734ba3a9d30ee16ecf
Lauri Larjo <laurilarjo@gmail.com>
2024-02-12 22:56:21 +0200

    Fix the number formatting issues with excel by not using numbers

--- src/lib/exporter.ts
+++ src/lib/exporter.ts
@@ -8,15 +8,6 @@ export function exportToXlsx(transactions: Transaction[], filename: string) {
   console.log('Exporting to xlsx...');
   const wb = xlsx.utils.book_new();
   const ws = xlsx.utils.json_to_sheet(transactionsToWrite);
-  
-  // Set columns 3 & 4 to be number formatted without decimal in thousands
-  for (let row = 1; row <= transactions.length + 1; row++) {
-    const cellRef1 = xlsx.utils.encode_cell({r: row - 1, c: 3}); // c: 1 for column B, rows are 0-indexed
-    if(ws[cellRef1]) ws[cellRef1].z = '0';
-    
-    const cellRef2 = xlsx.utils.encode_cell({r: row - 1, c: 4}); // c: 1 for column B, rows are 0-indexed
-    if(ws[cellRef2]) ws[cellRef2].z = '0';
-  }
 
   xlsx.utils.book_append_sheet(wb, ws, 'Transactions');
   xlsx.writeFile(wb, filename);
@@ -24,6 +15,15 @@ export function exportToXlsx(transactions: Transaction[], filename: string) {
 
 function mapTransactionsToRows(transactions: Transaction[]) {
   return transactions.map(x => {
-    return [x.month, x.year, x.date, x.amount, x.amountEur, x.payee, x.transactionType, x.message];
+    return [
+      x.month, 
+      x.year, 
+      x.date, 
+      // perkeleen excel. convert numbers to strings because excel is a piece of ...
+      x.amount.toString().replace(',', ''), // the thousands separator is a comma
+      x.amountEur.toString().replace(',', ''), // the thousands separator is a comma
+      x.payee, 
+      x.transactionType, 
+      x.message];
   });
 }
