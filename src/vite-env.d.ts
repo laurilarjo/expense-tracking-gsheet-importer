8f5477374498c52556744dc081a5ffecc2110e3d
Lauri Larjo <laurilarjo@gmail.com>
2026-07-02 16:06:04 +0300

    temp

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,7 +4,15 @@
 declare const __BUILD_DATE__: string;
 
 interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
   readonly VITE_FIREBASE_FUNCTIONS_URL: string;
+  readonly VITE_GOOGLE_API_KEY: string;
+  readonly VITE_GOOGLE_CLIENT_ID: string;
 }
 
 interface ImportMeta {
